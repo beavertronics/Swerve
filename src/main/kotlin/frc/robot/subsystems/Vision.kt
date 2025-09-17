0631d70928e80fe3c8fec88f2134a5dae080a25d
Sketched Doughnut <noreply>
2025-09-16 22:26:09 -0700

    added fallback to vision, fixed pose updating, added april tags for bunnybots 2025?

--- src/main/kotlin/frc/robot/subsystems/Vision.kt
+++ src/main/kotlin/frc/robot/subsystems/Vision.kt
@@ -8,17 +8,22 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout
 import edu.wpi.first.apriltag.AprilTagFields
 import edu.wpi.first.math.geometry.Rotation3d
 import edu.wpi.first.math.geometry.Transform3d
+import edu.wpi.first.math.geometry.Translation3d
 import org.photonvision.PhotonPoseEstimator
 
 val Vision = BeaverPhotonVision(
     VisionCamera(
-        "victoiA", // todo
-        Transform3d(
-            -12.5.inches.asMeters, 7.0.inches.asMeters, 22.5.inches.asMeters, Rotation3d(
+        name = "victoiA",
+        // 12.5 inches backwards, 7.0 inches right, 22.5 inches up on the robot
+        robotToCamera = Transform3d(
+            -12.5.inches.asMeters, -7.0.inches.asMeters, 22.5.inches.asMeters,
+            Rotation3d(
                 0.0.degrees.asRadians, 0.0.degrees.asRadians, 0.0.degrees.asRadians
             )
         ),
-        AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField),
-        PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR
+        layout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField),
+        strategy = PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+        fallbackStrategy = PhotonPoseEstimator.PoseStrategy.CLOSEST_TO_REFERENCE_POSE
+
     )
 )
