33c87e53956d872f8a3bd820eea81012d291dd40
Will Kam <william@kamelot.me>
2025-09-18 16:59:32 -0700

    fixes, vision works!
    lowered controller deadband to 0.125, custom field is breaking vision, loaded crescendo layout instead

--- src/main/kotlin/frc/robot/subsystems/Vision.kt
+++ src/main/kotlin/frc/robot/subsystems/Vision.kt
@@ -4,6 +4,8 @@ import Engine.BeaverPhotonVision
 import Engine.VisionCamera
 import beaverlib.utils.Units.Angular.degrees
 import beaverlib.utils.Units.Linear.inches
+import edu.wpi.first.apriltag.AprilTagFieldLayout
+import edu.wpi.first.apriltag.AprilTagFields
 import edu.wpi.first.math.geometry.Rotation3d
 import edu.wpi.first.math.geometry.Transform3d
 import frc.robot.subsystems.`according to all known laws of aviation, our robot should not be able to fly`.customFieldLayout
@@ -19,7 +21,7 @@ val Vision = BeaverPhotonVision(
                 0.0.degrees.asRadians, 0.0.degrees.asRadians, 0.0.degrees.asRadians
             )
         ),
-        layout = customFieldLayout,
+        layout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo),
         strategy = PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
         fallbackStrategy = PhotonPoseEstimator.PoseStrategy.CLOSEST_TO_REFERENCE_POSE
 
