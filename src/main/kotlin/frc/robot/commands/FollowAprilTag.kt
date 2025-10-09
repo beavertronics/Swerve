566ec491c96bc0bffb7f719497b958309e1374d6
Will Kam <william@kamelot.me>
2025-10-09 16:54:41 -0700

    follow works now, bit shaky still

--- src/main/kotlin/frc/robot/commands/FollowAprilTag.kt
+++ src/main/kotlin/frc/robot/commands/FollowAprilTag.kt
@@ -1,6 +1,9 @@
 package frc.robot.commands
 
 import beaverlib.utils.Sugar.clamp
+import beaverlib.utils.Units.Angular.asDegrees
+import beaverlib.utils.Units.Angular.degrees
+import beaverlib.utils.Units.Angular.radians
 import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
@@ -11,7 +14,7 @@ import org.photonvision.targeting.PhotonTrackedTarget
 import kotlin.math.sign
 
 class FollowAprilTag(val aprilTagID: Int, val speedLimit: Double = 1.0) : Command() {
-    val rotateKP = 1.0
+    val rotateKP = 0.5
     val xKP = 1.0
     val yKP = 1.0
     val rotatePID = PIDController(rotateKP, 0.0, 0.0)
@@ -20,14 +23,14 @@ class FollowAprilTag(val aprilTagID: Int, val speedLimit: Double = 1.0) : Comman
     // add the needed subsystems to requirements
     init {
         addRequirements(Drivetrain)
-        rotatePID.setpoint = 180.0 // degrees
-        xPID.setpoint = 1.0 // meters
+        rotatePID.setpoint = 0.0 // degrees
+        xPID.setpoint = 2.0 // meters
         yPID.setpoint = 0.0 // meters
 
-        // give it some deadzone that rotation is ok
-        rotatePID.setTolerance(0.025)
-        xPID.setTolerance(0.025)
-        yPID.setTolerance(0.025)
+        // give it some deadzone
+        rotatePID.setTolerance(5.0) // deadzone of 5 degrees
+        xPID.setTolerance(0.5)
+        yPID.setTolerance(0.5)
     }
 
     // get the correct april tag
@@ -56,25 +59,25 @@ class FollowAprilTag(val aprilTagID: Int, val speedLimit: Double = 1.0) : Comman
         // 180 degrees is facing tag, so gets difference from 180 - current rotation
         // also, rotation2D handles the issue with a half-turn rotation and wraps to 360 (?)
         val rotateError = Rotation2d.fromDegrees(180.0).minus(Rotation2d.fromRadians(yawToTag)).radians
-        val driveRotate = ((rotatePID.calculate(rotateError) * 1.0) +
+        val driveRotate = ((rotatePID.calculate(rotateError) * -1.0) +
                 (-0.01 * rotateError.sign)).clamp(-1.0 * speedLimit, speedLimit) // gives it a little more kick towards 0 error (constant)
 
 
         // generate a value to align with the tag via x (forwards/backwards)
         val xToTag = desiredTag!!.bestCameraToTarget.x
-        val xError = xToTag - 2.0 // current - goal of 1 meter
-        val driveX = ((xPID.calculate(xError) * -1.0) +
+        val xError = xToTag
+        val driveX = ((xPID.calculate(xError)) +
                 (-0.01 * xError.sign)).clamp(-1.0 * speedLimit, speedLimit) // gives it a little more kick towards 0 error (constant)
 
 
         // generate a value to align with the tag via y (left/right)
         val yToTag = desiredTag!!.bestCameraToTarget.y
-        val yError = yToTag // current - goal of 1 meter
-        val driveY = ((yPID.calculate(yError) * -1.0) +
-                (0.01 * yError.sign)).clamp(-1.0 * speedLimit, speedLimit) // gives it a little more kick towards 0 error (constant)
+        val yError = yToTag
+        val driveY = ((yPID.calculate(yError)) +
+                (-0.01 * yError.sign)).clamp(-1.0 * speedLimit, speedLimit) // gives it a little more kick towards 0 error (constant)
 
         // add everything into a chassis speed and drive robot
-        Drivetrain.drive(ChassisSpeeds(0.0, 0.0, driveRotate)) // in m/s and radians/s
+        Drivetrain.drive(ChassisSpeeds(driveX, driveY, driveRotate)) // in m/s and radians/s
     }
 
 
